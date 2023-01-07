144d51e4f5db4e60d7e3d836f73c9409c1d712c1
Simon Kallweit <simon.kallweit@gmail.com>
2023-01-07 22:18:58 +0100

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
+#include <OpenImageIO/imageio.h>
 #include <fmt/format.h>
 #include <spdlog/spdlog.h>
+
+#include <filesystem>
 
 int main() {
     spdlog::info("just a test {}", 123);
 
-    fmt::print("{} {}\n", "hello", "world");
+    std::filesystem::path root_dir{"C:/projects/fotorite/photos"};
+
+    for (const auto it : std::filesystem::directory_iterator{root_dir}) {
+        if (it.is_regular_file()) {
+            spdlog::info("reading {}", it.path());
+
+            auto inp = OIIO::ImageInput::open(it.path());
+            if (!inp)
+                continue;
+            const OIIO::ImageSpec &spec = inp->spec();
+            int xres = spec.width;
+            int yres = spec.height;
+            int channels = spec.nchannels;
+            std::vector<float> pixels(xres * yres * channels);
+            spdlog::info("image data size {}", pixels.size());
+            inp->read_image(OIIO::TypeDesc::FLOAT, &pixels[0]);
+            inp->close();
+        }
+    }
+
     return 0;
 }
