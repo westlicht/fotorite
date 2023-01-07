5aca3b061929985b5426e1cc84fed3a3e8bd2a3c
Simon Kallweit <simon.kallweit@gmail.com>
2023-01-07 22:39:56 +0100

    cutdown deps

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
-#include <OpenImageIO/imageio.h>
-#include <fmt/format.h>
+#include <fmt/std.h>
 #include <spdlog/spdlog.h>
 
 #include <filesystem>
@@ -13,17 +12,17 @@ int main() {
         if (it.is_regular_file()) {
             spdlog::info("reading {}", it.path());
 
-            auto inp = OIIO::ImageInput::open(it.path());
-            if (!inp)
-                continue;
-            const OIIO::ImageSpec &spec = inp->spec();
-            int xres = spec.width;
-            int yres = spec.height;
-            int channels = spec.nchannels;
-            std::vector<float> pixels(xres * yres * channels);
-            spdlog::info("image data size {}", pixels.size());
-            inp->read_image(OIIO::TypeDesc::FLOAT, &pixels[0]);
-            inp->close();
+            // auto inp = OIIO::ImageInput::open(it.path());
+            // if (!inp)
+            //     continue;
+            // const OIIO::ImageSpec &spec = inp->spec();
+            // int xres = spec.width;
+            // int yres = spec.height;
+            // int channels = spec.nchannels;
+            // std::vector<float> pixels(xres * yres * channels);
+            // spdlog::info("image data size {}", pixels.size());
+            // inp->read_image(OIIO::TypeDesc::FLOAT, &pixels[0]);
+            // inp->close();
         }
     }
 
